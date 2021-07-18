9f4dd61a2f0256185747ef64583391a3fdf0fac4
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-07-18 11:46:14 +0700

    set up the folder structure
    deleted the node_modules and build folder

---
+++ ganace_deploy/contracts/ChannelApi.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.4.26 < 0.7.2;
+
+contract ChannelApi {
+    function applyRuntimeUpdate(address from, address to, uint impressionsCount, uint fraudCount);
+
+    function applyAuditorsCheckUpdate(address from, address to, uint fraudCountDelta);
+}
