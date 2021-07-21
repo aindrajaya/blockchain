e1072fec3669b0d61ba0020cbe8b6ed390f735d9
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-07-21 22:17:07 +0700

    java blockchain simple implementation

---
+++ blockchain-java/java/java-blockchain/src/main/java/com/aindrajaya/Block.java
@@ -0,0 +1,62 @@
+package com.aindrajaya.Block;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Block {
+  private static Logger logger = Logger.getLogger(Block.class.getName());
+
+  private String hash;
+  private String previousHash;
+  private String data;
+  private long timeStamp;
+  private int nonce;
+
+  public Block(String data, String previousHash, long timeStamp){
+    this.data = data;
+    this.previousHash = previousHash;
+    this.timeStamp = timeStamp;
+    this.hash = calculateBlockHash();
+  }
+
+  public String mineBlock(int prefix){
+    String prefixString = new String(new char[prefix]).replace('\0', '0');
+    while(!hash.substring(0, preix)
+      .equals(prefixString)){
+        nonce++;
+        hash = calculateBlockHash();
+      }
+  }
+
+  public String calculateBlockHash(){
+    String dataToHash = previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data;
+    MessageDigest digest =null;
+    byte[] bytes = null;
+    try{
+      digest = MessageDigest.getInstance("SHA-256");
+      bytes = digest.digest(dataToHash.getBytes("UTF-8"));
+    } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex){
+      logger.log(Level.SEVERE, ex.getMessage());
+    }
+    StringBuffer buffer = new StringBuffer();
+    for(byte b : bytes){
+      buffer.append(String.format("%02x", b));
+    }
+    return buffer.toString();
+  }
+
+  public String getHash(){
+    return this.hash;
+  }
+
+  public String getPreviousHash(){
+    return this.getPreviousHash;
+  }
+
+  public void setData(String data){
+    this.data = data;
+  }
+}
