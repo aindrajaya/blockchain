01a9b3b5a7a305ee2976cc05d7d2fa0269bd7b61
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-09-02 14:37:00 +0700

    little update and add notes
    update the code from counter.js interface and add notes for each functions

--- ethereum-for-webdev/ch2-counter-app/src/contracts/Counter.js
+++ ethereum-for-webdev/ch2-counter-app/src/contracts/Counter.js
@@ -1,16 +1,18 @@
-import Artifacts from './combined.json'
+import Artifacts from './combined.json' //artifacts is copy from the build folder
 import { getWeb3, getAccount } from '../eth/network';
 
+//code for create a Counter web3 contrat object, it just creates a javascript object that acts as a wrapper to a contract previously deployed at a specified address
 export default function Counter(web3, address, options = {}){
   const name = "contracts/Counter.sol:Counter";
-  const artifacts = Artifacts.contracts[name];
-  const abi = JSON.parse(artifacts.abi)
+  const artifact = Artifacts.contracts[name];
+  const abi = JSON.parse(artifact.abi)
   return new web3.eth.Contract(abi, address, options);
 }
 
+//code for obtaining a web3 contract instance of the Counter contract
 export async function getDeployed(){
   const web3 = getWeb3();
   const from = await getAccount();
-  const addr = "address";
+  const addr = "0x33719B186Eb764e6F47345bE01e284aD807399a5";
   return Counter(web3, addr, {from});
 }
--- ethereum-for-webdev/ch2-counter-app/src/eth/network.js
+++ ethereum-for-webdev/ch2-counter-app/src/eth/network.js
@@ -1,19 +1,24 @@
 import Web3 from 'web3';
 
+//creating a web3 object using MetaMask provider
 let web3;
 export function getWeb3(){
-  if(!Web3){
+  if(!web3){
+    //create the new Instance web3 provider
     web3 = new Web3(Web3.givenProvider);
   }
   return web3;
 }
 
+//function to retrieving the user's current default account, usually using the 0 index account
 export async function getAccount(){
   const web3 = getWeb3();
   const accounts  = await web3.eth.getAccounts();
   return accounts[0]
 
-  //for the privacy mode
-  const accounts1 = await window.ethereum.enable();
-  return accounts1[0];
+  //for handling the privacy mode browser
+  /**
+   * const accounts1 = await window.ethereum.enable();
+   * return accounts1[0];
+   */
 }
