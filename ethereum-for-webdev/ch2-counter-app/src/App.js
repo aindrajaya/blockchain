5d0c14dbb75538f92e8df8582fd8aa86b4d00a36
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-09-02 15:17:00 +0700

    call the state from Counter contract interface
    call the state from getDeployed function from Counter interface that can consume state/data from blockchain

--- ethereum-for-webdev/ch2-counter-app/src/App.js
+++ ethereum-for-webdev/ch2-counter-app/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import React, {Component} from 'react';
 import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-     <Counter />
-    </div>
-  );
+//import the getDeployed function from the Counter contract interface
+import { getDeployed } from './contracts/Counter';
+
+
+class App extends Component {
+  state = {counter: null}
+
+  //will get the state from the deployed Counter contracts
+  async componentDidMount(){
+    const counter = await getDeployed()
+    this.setState({counter})
+  }
+
+  render(){
+    const {counter} = this.state;
+    return (
+      <div className="App">
+        {/* checkt the counter state, if not null show the Counter component */}
+        {counter && <Counter contract={counter}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
