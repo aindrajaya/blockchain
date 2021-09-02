b8ba4cc1555e8f2ce1c20be890109016d0480f44
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-09-02 12:57:00 +0700

    create counter components
    create counter and import it on the app.js

--- ethereum-for-webdev/ch2-counter-app/src/App.js
+++ ethereum-for-webdev/ch2-counter-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      React App
+     <Counter />
     </div>
   );
 }
---
+++ ethereum-for-webdev/ch2-counter-app/src/components/Counter.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Counter extends Component {
+  render(){
+    return(
+      <div>
+        Counter should be here
+      </div>
+    )
+  }
+}
+
+export default Counter
