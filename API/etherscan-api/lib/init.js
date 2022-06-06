5220c281073e1df468d28e6e1df9aab94efc295d
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-06-07 04:00:00 +0700

    add file - init.js #1
    first initiation file that contain function to provide an API service from etherscan

---
+++ API/etherscan-api/lib/init.js
@@ -0,0 +1,75 @@
+"use strict"
+const axios = require('axios');
+
+//Libs called
+// const log =  require('./log')
+// const proxy = require('./proxy');
+// const stats = require('./stats');
+// const block = require('./block');
+// const transaction = require('./transaction');
+// const contract = require('./contract');
+// const account = require('./account');
+// const pickChainUrl = require('./pickChainUrl');
+
+/**
+ * @module etherscan/api
+ */
+
+/**
+ * @param {string} apiKey - (optional) Your Etherscan APIkey
+ * @param {string} chain - (optional) Other chain keys [ropsten, rinkeby, kovan]
+ * @param {number} timeout - (optional) Timeout in miliseconds for request, default 10000
+ * @param {object} client - optional axios client instance
+ */
+
+module.exports = function(apiKey, chain, timeout, client = null) {
+  if(!apiKey){
+    apiKey = 'YourAPIKeyToken';
+  }
+
+  if(!timeout){
+    timeout = 10000;
+  }
+
+  if(!client){
+    client = axios.create({
+      baseUrl: pickChainUrl(chain),
+      timeout: timeout
+    });
+  }
+
+  // var getRequest = requre('./getRequest')(chain, timeout, client);
+  /**
+   * @lends module:etherscan/api
+   */
+  return{
+    /**
+     * @namespace
+     */
+    // log: log(getRequest, apiKey),
+    /**
+     * @namespace
+     */
+    // proxy: proxy(getRequest, apiKey),
+    /**
+     * @namespace
+     */
+    // stats: stats(getRequest, apiKey),
+    /**
+     * @namespace
+     */
+    // block: block(getRequest, apiKey),
+    /**
+     * @namespace
+     */
+    // transaction: transaction(getRequest, apiKey),
+    /**
+     * @namespace
+     */
+    // contract: contract(getRequest, apiKey);
+    /**
+     * @namespace
+     */
+    // account: account(getRequest, apiKey);
+  };
+};
